b9913ef033211907dc4001fc9848b11882e699ad
Piotr Piech <tenemo@gmail.com>
2019-12-08 19:32:59 +0100

    prettier wants to just remove a bracket

--- src/SomeComponent/SomeComponent.jsx
+++ src/SomeComponent/SomeComponent.jsx
@@ -1,20 +1,21 @@
-import React from 'react';
+import React from "react";
 
 const SomeComponent = () => {
   const veryLongFunctionNameOmgDogeSoLongWhyIsThisNotEndingWtfWhoNamesVariables = () => {};
   return (
-         <div
-            someObjProp={{
-            objProp: {
-                internalProperty: {
-                    someFunction: function() {
-                        return veryLongFunctionNameOmgDogeSoLongWhyIsThisNotEndingWtfWhoNamesVariables();
-                        },
-                    }
-            },
-            }}
-        >bugged</div>
-    )
-}
+    <div
+      someObjProp={{
+        objProp: {
+          internalProperty: {
+            someFunction() {
+              return veryLongFunctionNameOmgDogeSoLongWhyIsThisNotEndingWtfWhoNamesVariables();
+                        }
+        }
+      }}
+    >
+      bugged
+    </div>
+  );
+};
 
 export default SomeComponent;
